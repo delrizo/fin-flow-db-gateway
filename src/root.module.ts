f31267a778e70551dd33901799d2257b19968b4d
Александр Герасимович <sollentson@gmail.com>
2026-02-22 03:45:27 +0300

    Update

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigRootModule } from '~/root/config.root.module'
 import { SequelizeRootModule } from '~/root/sequelize.root.module'
+import { UsersModule } from '~/modules/user/user.module'
 
 @Module({
-    imports: [ConfigRootModule, SequelizeRootModule]
+    imports: [ConfigRootModule, SequelizeRootModule, UsersModule]
 })
 export class RootModule {}
